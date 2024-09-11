731204eec2ab3927e0f4b5f1a603453cfb1fcf83
lvhan028 <lvhan_028@163.com>
2024-09-11 17:55:31 +0800

    merge dev

--- demo/demo.py
+++ demo/demo.py
@@ -1,5 +1,6 @@
 import os
 import json
+import fire
 
 from loguru import logger
 
@@ -9,30 +10,80 @@ from magic_pdf.rw.DiskReaderWriter import DiskReaderWriter
 import magic_pdf.model as model_config 
 model_config.__use_inside_model__ = True
 
-try:
+# try:
+#     current_script_dir = os.path.dirname(os.path.abspath(__file__))
+#     demo_name = "demo1"
+#     pdf_path = os.path.join(current_script_dir, f"{demo_name}.pdf")
+#     model_path = os.path.join(current_script_dir, f"{demo_name}.json")
+#     pdf_bytes = open(pdf_path, "rb").read()
+#     # model_json = json.loads(open(model_path, "r", encoding="utf-8").read())
+#     model_json = []  # model_json传空list使用内置模型解析
+#     jso_useful_key = {"_pdf_type": "", "model_list": model_json}
+#     local_image_dir = os.path.join(current_script_dir, 'images')
+#     image_dir = str(os.path.basename(local_image_dir))
+#     image_writer = DiskReaderWriter(local_image_dir)
+#     pipe = UNIPipe(pdf_bytes, jso_useful_key, image_writer)
+#     pipe.pipe_classify()
+#     """如果没有传入有效的模型数据，则使用内置model解析"""
+#     if len(model_json) == 0:
+#         if model_config.__use_inside_model__:
+#             pipe.pipe_analyze()
+#         else:
+#             logger.error("need model list input")
+#             exit(1)
+#     pipe.pipe_parse()
+#     md_content = pipe.pipe_mk_markdown(image_dir, drop_mode="none")
+#     with open(f"{demo_name}.md", "w", encoding="utf-8") as f:
+#         f.write(md_content)
+# except Exception as e:
+#     logger.exception(e)
+
+def process(pdf_list):
+    import pdb; pdb.set_trace()
     current_script_dir = os.path.dirname(os.path.abspath(__file__))
-    demo_name = "demo1"
-    pdf_path = os.path.join(current_script_dir, f"{demo_name}.pdf")
-    model_path = os.path.join(current_script_dir, f"{demo_name}.json")
-    pdf_bytes = open(pdf_path, "rb").read()
-    # model_json = json.loads(open(model_path, "r", encoding="utf-8").read())
-    model_json = []  # model_json传空list使用内置模型解析
-    jso_useful_key = {"_pdf_type": "", "model_list": model_json}
-    local_image_dir = os.path.join(current_script_dir, 'images')
-    image_dir = str(os.path.basename(local_image_dir))
-    image_writer = DiskReaderWriter(local_image_dir)
-    pipe = UNIPipe(pdf_bytes, jso_useful_key, image_writer)
-    pipe.pipe_classify()
-    """如果没有传入有效的模型数据，则使用内置model解析"""
-    if len(model_json) == 0:
-        if model_config.__use_inside_model__:
-            pipe.pipe_analyze()
-        else:
-            logger.error("need model list input")
-            exit(1)
-    pipe.pipe_parse()
-    md_content = pipe.pipe_mk_markdown(image_dir, drop_mode="none")
-    with open(f"{demo_name}.md", "w", encoding="utf-8") as f:
-        f.write(md_content)
-except Exception as e:
-    logger.exception(e)
+    
+    for index, pdf_path in enumerate(pdf_list):
+        pdf_bytes = open(pdf_path, "rb").read()
+   
+        model_json = []  # model_json传空list使用内置模型解析
+        
+        jso_useful_key = {"_pdf_type": "", "model_list": model_json}
+        local_image_dir = os.path.join(current_script_dir, 'images')
+        image_dir = str(os.path.basename(local_image_dir))
+        image_writer = DiskReaderWriter(local_image_dir)
+        pipe = UNIPipe(pdf_bytes, jso_useful_key, image_writer)
+        pipe.pipe_classify()
+        """如果没有传入有效的模型数据，则使用内置model解析"""
+        if len(model_json) == 0:
+            if model_config.__use_inside_model__:
+                pipe.pipe_analyze()
+            else:
+                logger.error("need model list input")
+                exit(1)
+        pipe.pipe_parse()
+        md_content = pipe.pipe_mk_markdown(image_dir, drop_mode="none")
+        with open(f"results/{index}.md", "w", encoding="utf-8") as f:
+            f.write(md_content)
+
+def file_ext(file_path):
+    return os.path.splitext(file_path)[-1]
+
+
+def main(file_path: str):
+    pdf_list = []
+    ext = file_ext(file_path)
+    if ext == '.pdf':
+        pdf_list.append(file_path)
+        pdf_list *= 100
+    elif ext == '.txt':
+        with open(file_path) as f:
+            file_paths = f.readlines()
+            pdf_list += [_path for _path in file_paths if file_ext(_path) == '.pdf']
+    else:
+        print(f'unexpected input file_path: {file_path}')
+        exit(-1)
+    
+    process(pdf_list)
+
+if __name__ == '__main__':
+    fire.Fire(main)
--- magic_pdf/model/pdf_extract_kit.py
+++ magic_pdf/model/pdf_extract_kit.py
@@ -59,7 +59,7 @@ def mfd_model_init(weight):
 def mfr_model_init(weight_dir, cfg_path, _device_='cpu'):
     args = argparse.Namespace(cfg_path=cfg_path, options=None)
     cfg = Config(args)
-    cfg.config.model.pretrained = os.path.join(weight_dir, "pytorch_model.pth")
+    cfg.config.model.pretrained = os.path.join(weight_dir, "unimernet_base.pth")
     cfg.config.model.model_config.model_name = weight_dir
     cfg.config.model.tokenizer_config.path = weight_dir
     task = tasks.setup_task(cfg)
@@ -333,91 +333,91 @@ class CustomPEKModel:
             return_list = [crop_paste_x, crop_paste_y, crop_xmin, crop_ymin, crop_xmax, crop_ymax, crop_new_width, crop_new_height]
             return return_image, return_list
 
-        pil_img = Image.fromarray(image)
+        # pil_img = Image.fromarray(image)
 
-        # ocr识别
-        if self.apply_ocr:
-            ocr_start = time.time()
-            # Process each area that requires OCR processing
-            for res in ocr_res_list:
-                new_image, useful_list = crop_img(res, pil_img, crop_paste_x=50, crop_paste_y=50)
-                paste_x, paste_y, xmin, ymin, xmax, ymax, new_width, new_height = useful_list
-                # Adjust the coordinates of the formula area
-                adjusted_mfdetrec_res = []
-                for mf_res in single_page_mfdetrec_res:
-                    mf_xmin, mf_ymin, mf_xmax, mf_ymax = mf_res["bbox"]
-                    # Adjust the coordinates of the formula area to the coordinates relative to the cropping area
-                    x0 = mf_xmin - xmin + paste_x
-                    y0 = mf_ymin - ymin + paste_y
-                    x1 = mf_xmax - xmin + paste_x
-                    y1 = mf_ymax - ymin + paste_y
-                    # Filter formula blocks outside the graph
-                    if any([x1 < 0, y1 < 0]) or any([x0 > new_width, y0 > new_height]):
-                        continue
-                    else:
-                        adjusted_mfdetrec_res.append({
-                            "bbox": [x0, y0, x1, y1],
-                        })
+        # # ocr识别
+        # if self.apply_ocr:
+        #     ocr_start = time.time()
+        #     # Process each area that requires OCR processing
+        #     for res in ocr_res_list:
+        #         new_image, useful_list = crop_img(res, pil_img, crop_paste_x=50, crop_paste_y=50)
+        #         paste_x, paste_y, xmin, ymin, xmax, ymax, new_width, new_height = useful_list
+        #         # Adjust the coordinates of the formula area
+        #         adjusted_mfdetrec_res = []
+        #         for mf_res in single_page_mfdetrec_res:
+        #             mf_xmin, mf_ymin, mf_xmax, mf_ymax = mf_res["bbox"]
+        #             # Adjust the coordinates of the formula area to the coordinates relative to the cropping area
+        #             x0 = mf_xmin - xmin + paste_x
+        #             y0 = mf_ymin - ymin + paste_y
+        #             x1 = mf_xmax - xmin + paste_x
+        #             y1 = mf_ymax - ymin + paste_y
+        #             # Filter formula blocks outside the graph
+        #             if any([x1 < 0, y1 < 0]) or any([x0 > new_width, y0 > new_height]):
+        #                 continue
+        #             else:
+        #                 adjusted_mfdetrec_res.append({
+        #                     "bbox": [x0, y0, x1, y1],
+        #                 })
 
-                # OCR recognition
-                new_image = cv2.cvtColor(np.asarray(new_image), cv2.COLOR_RGB2BGR)
-                ocr_res = self.ocr_model.ocr(new_image, mfd_res=adjusted_mfdetrec_res)[0]
+        #         # OCR recognition
+        #         new_image = cv2.cvtColor(np.asarray(new_image), cv2.COLOR_RGB2BGR)
+        #         ocr_res = self.ocr_model.ocr(new_image, mfd_res=adjusted_mfdetrec_res)[0]
 
-                # Integration results
-                if ocr_res:
-                    for box_ocr_res in ocr_res:
-                        p1, p2, p3, p4 = box_ocr_res[0]
-                        text, score = box_ocr_res[1]
+        #         # Integration results
+        #         if ocr_res:
+        #             for box_ocr_res in ocr_res:
+        #                 p1, p2, p3, p4 = box_ocr_res[0]
+        #                 text, score = box_ocr_res[1]
 
-                        # Convert the coordinates back to the original coordinate system
-                        p1 = [p1[0] - paste_x + xmin, p1[1] - paste_y + ymin]
-                        p2 = [p2[0] - paste_x + xmin, p2[1] - paste_y + ymin]
-                        p3 = [p3[0] - paste_x + xmin, p3[1] - paste_y + ymin]
-                        p4 = [p4[0] - paste_x + xmin, p4[1] - paste_y + ymin]
+        #                 # Convert the coordinates back to the original coordinate system
+        #                 p1 = [p1[0] - paste_x + xmin, p1[1] - paste_y + ymin]
+        #                 p2 = [p2[0] - paste_x + xmin, p2[1] - paste_y + ymin]
+        #                 p3 = [p3[0] - paste_x + xmin, p3[1] - paste_y + ymin]
+        #                 p4 = [p4[0] - paste_x + xmin, p4[1] - paste_y + ymin]
 
-                        layout_res.append({
-                            'category_id': 15,
-                            'poly': p1 + p2 + p3 + p4,
-                            'score': round(score, 2),
-                            'text': text,
-                        })
+        #                 layout_res.append({
+        #                     'category_id': 15,
+        #                     'poly': p1 + p2 + p3 + p4,
+        #                     'score': round(score, 2),
+        #                     'text': text,
+        #                 })
 
-            ocr_cost = round(time.time() - ocr_start, 2)
-            logger.info(f"ocr cost: {ocr_cost}")
+        #     ocr_cost = round(time.time() - ocr_start, 2)
+        #     logger.info(f"ocr cost: {ocr_cost}")
 
-        # 表格识别 table recognition
-        if self.apply_table:
-            table_start = time.time()
-            for res in table_res_list:
-                new_image, _ = crop_img(res, pil_img)
-                single_table_start_time = time.time()
-                logger.info("------------------table recognition processing begins-----------------")
-                latex_code = None
-                html_code = None
-                if self.table_model_type == STRUCT_EQTABLE:
-                    with torch.no_grad():
-                        latex_code = self.table_model.image2latex(new_image)[0]
-                else:
-                    html_code = self.table_model.img2html(new_image)
+        # # 表格识别 table recognition
+        # if self.apply_table:
+        #     table_start = time.time()
+        #     for res in table_res_list:
+        #         new_image, _ = crop_img(res, pil_img)
+        #         single_table_start_time = time.time()
+        #         logger.info("------------------table recognition processing begins-----------------")
+        #         latex_code = None
+        #         html_code = None
+        #         if self.table_model_type == STRUCT_EQTABLE:
+        #             with torch.no_grad():
+        #                 latex_code = self.table_model.image2latex(new_image)[0]
+        #         else:
+        #             html_code = self.table_model.img2html(new_image)
 
-                run_time = time.time() - single_table_start_time
-                logger.info(f"------------table recognition processing ends within {run_time}s-----")
-                if run_time > self.table_max_time:
-                    logger.warning(f"------------table recognition processing exceeds max time {self.table_max_time}s----------")
-                # 判断是否返回正常
+        #         run_time = time.time() - single_table_start_time
+        #         logger.info(f"------------table recognition processing ends within {run_time}s-----")
+        #         if run_time > self.table_max_time:
+        #             logger.warning(f"------------table recognition processing exceeds max time {self.table_max_time}s----------")
+        #         # 判断是否返回正常
 
-                if latex_code:
-                    expected_ending = latex_code.strip().endswith('end{tabular}') or latex_code.strip().endswith(
-                        'end{table}')
-                    if expected_ending:
-                        res["latex"] = latex_code
-                    else:
-                        logger.warning(f"------------table recognition processing fails----------")
-                elif html_code:
-                    res["html"] = html_code
-                else:
-                    logger.warning(f"------------table recognition processing fails----------")
-            table_cost = round(time.time() - table_start, 2)
-            logger.info(f"table cost: {table_cost}")
+        #         if latex_code:
+        #             expected_ending = latex_code.strip().endswith('end{tabular}') or latex_code.strip().endswith(
+        #                 'end{table}')
+        #             if expected_ending:
+        #                 res["latex"] = latex_code
+        #             else:
+        #                 logger.warning(f"------------table recognition processing fails----------")
+        #         elif html_code:
+        #             res["html"] = html_code
+        #         else:
+        #             logger.warning(f"------------table recognition processing fails----------")
+        #     table_cost = round(time.time() - table_start, 2)
+        #     logger.info(f"table cost: {table_cost}")
 
         return layout_res
